fe0ff59f7db785ded502f6c7af1aaf55e186eaf2
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 17:46:59 -0700

    added user input functionality

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,5 +1,6 @@
 def file_to_dict(filename):
-# open file
+    """opens the file and returns a dictionary of restaurants and their ratings"""
+
     file_contents = open(filename)
     Yelp = {}
 # loop through file
@@ -11,25 +12,46 @@ def file_to_dict(filename):
     return Yelp
     file_contents.close() #crazy!  we have to close our variable, not the filename!
 
-
-# def printed_clealy(D):
-#     for rest, score in D.items():
-
-
-#         print "%s is rated at: %d" %(rest, score)
-
-
 def printed_clearly(D):
+    """Prints the contents of a dictionary in alphabetical order and includes the rating"""
+
     alpha_rest = sorted(D)
-    #sort the dictionary that gives us a list of keys
-    # now we have an alphabetical list (but no values)
+
     for rest_name in alpha_rest:
         print "%s is rated at: %d" % (rest_name, D[rest_name])
-    #loop through list
-    # find each rest and query its value in dict
-    # add rest and dict_value to printed message
+
+def user_contribution():
+    user_feedback = {}
+    user_rest = raw_input("Please enter a restaurant: ")
+    user_rating = int(raw_input("Please enter a rating for that restaurant: "))
+    user_feedback[user_rest] = user_rating
+    return user_feedback
+    # create empty dict
+    # prompt user for restaurant and rating
+    # add that raw input to the dict
+    # return dict
+
+
+    #New function! to combine both dicts
 
 
 restaurant_ratings = file_to_dict("scores.txt")
 
-printed_clearly(restaurant_ratings)
+user_data = user_contribution()
+
+def dictionary_adder(d1, d2):
+    d1.update(d2)
+    return d1
+
+
+both = dictionary_adder(restaurant_ratings, user_data)
+
+printed_clearly(both)
+
+#printed_clearly(combined_restaraunt_data)
+
+# printed_clearly(restaurant_ratings)
+
+# printed_clearly(user_contribution())
+
+
